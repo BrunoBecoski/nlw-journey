27bd42435311a81bb0d6cc091442c4bcca20be47
Bruno Becoski <brunobecoski@gmail.com>
2024-07-12 21:08:47 -0300

    Cria a rota create-trip

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,28 +1,14 @@
 import fastify from "fastify";
-import { prisma } from "./lib/prisma";
+import { validatorCompiler, serializerCompiler } from "fastify-type-provider-zod";
+import { createTrip } from "./routes/create-trip";
 
 const app = fastify()
 
-app.get('/cadastrar', async () => {
-  await prisma.trip.create({
-    data: {
-      destination: 'Florianópolis',
-      starts_at: new Date(),
-      ends_at: new Date(),
-    }
-  })
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
-  return 'Registro cadastrado com sucesso!'
-})
-
-app.get('/listar', async () => {
-  const trips = await prisma.trip.findMany()
-
-  return trips
-})
+app.register(createTrip)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('🚀 Server running!')
 })
-
-console.log('Hello World')
