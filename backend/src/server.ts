f76bd00a43e6d62ca3d1dacd03be779488cd198c
Bruno Becoski <brunobecoski@gmail.com>
2024-07-17 22:12:32 -0300

    Cria as rotas create-link e get-links

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,8 +7,10 @@ import {
 import { confirmParticipant } from "./routes/confirm-participant"
 import { confirmTrip } from "./routes/confirm-trip"
 import { createActivity } from "./routes/create-activity"
+import { createLink } from "./routes/create-link"
 import { createTrip } from "./routes/create-trip"
 import { getActivities } from "./routes/get-activities"
+import { getLinks } from "./routes/get-links"
 
 const app = fastify()
 
@@ -24,6 +26,8 @@ app.register(confirmTrip)
 app.register(confirmParticipant)
 app.register(createActivity)
 app.register(getActivities)
+app.register(createLink)
+app.register(getLinks)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('🚀 Server running!')
