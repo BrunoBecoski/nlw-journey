bd4fb5684f5191510a885b2daa84c81a56c7476e
Bruno Becoski <brunobecoski@gmail.com>
2024-09-19 22:54:15 -0300

    Adiciona um participante

--- frontend/src/pages/trip-details/guests-modal.tsx
+++ frontend/src/pages/trip-details/guests-modal.tsx
@@ -2,6 +2,9 @@ import type { Participant } from "./guests"
 import { Modal } from "../../components/modal"
 import { Icon } from "../../components/icon"
 import { Button } from "../../components/button"
+import { FormEvent } from "react"
+import { useParams } from "react-router-dom"
+import { api } from "../../lib/axios"
 
 interface GuestsModalProps {
   closeGuestsModal: () => void
@@ -9,9 +12,30 @@ interface GuestsModalProps {
 }
 
 export function GuestsModal({ closeGuestsModal, participants }: GuestsModalProps) {
+  const { tripId } = useParams()
+
+  async function handleInvite(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const data = new FormData(event.currentTarget)
+    const email = data.get('email')?.toString()
+
+    if (!email) {
+      return
+    }
+
+    await api.post(`/trips/${tripId}/invites`, {
+      email,
+    })
+
+    window.document.location.reload()
+  }
+  
   return (
     <Modal
       title="Gerenciar Convidados"
+      description="Todos convidados podem visualizar as atividades."
+
       onClose={closeGuestsModal}
     >
       <div className="flex flex-col gap-6">
@@ -50,21 +74,21 @@ export function GuestsModal({ closeGuestsModal, participants }: GuestsModalProps
           })}
         </div>
 
-        <form className="p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
-        <div className="px-2 flex items-center flex-1 gap-2">
-          <Icon name="at-sign" className="text-zinc-400 size-5" />
-          <input
-            type="email"
-            name="email"
-            placeholder="Digite o e-mail do convidado"
-            className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-          />
-        </div>
+        <form onSubmit={handleInvite} className="p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+          <div className="px-2 flex items-center flex-1 gap-2">
+            <Icon name="at-sign" className="text-zinc-400 size-5" />
+            <input
+              type="email"
+              name="email"
+              placeholder="Digite o e-mail do convidado"
+              className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+            />
+          </div>
 
-        <Button type="submit" variant="primary" icon="plus">
-          Convidar
-        </Button>
-      </form>
+          <Button type="submit" variant="primary" icon="plus">
+            Convidar
+          </Button>
+        </form>
       </div>
     </Modal>
 
