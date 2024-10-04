c80dcc68f91a994bd161115ea1bb3e92d69edeb0
Bruno Becoski <brunobecoski@gmail.com>
2024-10-03 22:26:00 -0300

    Atualiza o componente Button

--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -4,12 +4,12 @@ import { tv, VariantProps } from "tailwind-variants"
 import { Icon, IconName } from "./icon"
 
 const buttonVariants = tv({
-  base: 'rounded-lg px-5 font-medium flex items-center justify-center gap-2',
+  base: 'rounded-lg px-5 font-medium flex items-center justify-center gap-2 disabled:opacity-50 disabled:cursor-not-allowed',
   
   variants: {
     variant: {
-      primary: 'bg-lime-300 text-lime-950 hover:bg-lime-400',
-      secondary: 'bg-zinc-800 text-zinc-200 hover:bg-zinc-700',
+      primary: 'bg-lime-300 text-lime-950 enabled:hover:bg-lime-400',
+      secondary: 'bg-zinc-800 text-zinc-200 enabled:hover:bg-zinc-700',
       close: '',
       icon: '',
     },
@@ -30,9 +30,10 @@ interface ButtonProps extends ComponentProps<'button'>, VariantProps<typeof butt
   icon?: IconName,
   iconSize?: 'md' | 'sm'
   children?: ReactNode,
+  isLoading?: boolean
 }
 
-export function Button({ icon, variant, size, iconSize = 'md', children, ...props }: ButtonProps) {
+export function Button({ icon, variant, size, iconSize = 'md', isLoading = false, children, ...props }: ButtonProps) {
 
   if (variant === 'close') {
     return (
@@ -51,9 +52,17 @@ export function Button({ icon, variant, size, iconSize = 'md', children, ...prop
   }
 
   return (
-    <button className={buttonVariants({ variant, size })} {...props}>
-      {children}
-      {icon && <Icon name={icon} className={ iconSize === 'md' ? 'size-5' : 'size-4'} />} 
+    <button className={buttonVariants({ variant, size })} disabled={isLoading} {...props}>
+      {
+        isLoading ? (
+          <Icon className="animate-spin" name="loader" />
+        ) : (
+          <>
+            {children}
+            {icon && <Icon name={icon} className={ iconSize === 'md' ? 'size-5' : 'size-4'} />} 
+          </>
+        )
+      }
     </button>
   )
 }
